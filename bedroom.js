19baafabf5cc0ca9cda0857a887b1b02658a2ce7
SuzzieM <100200764+SuzzieM@users.noreply.github.com>
2023-04-26 10:06:32 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,61 @@
+img = " ";
+status = " ";
+objects = [];
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+
+}
+
+function modelLoaded()
+{
+    console.log("Model is Loaded and Intialized!!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function preload()
+{
+   img = loadImage("Bedroom.jpg");
+}
+
+function draw()
+{
+    image (img, 0 , 0 , 640, 420);
+ //   R = random(255);
+ //   G = random(255);
+ //   B = random(225);
+    if(status != "")
+    {
+        for( i = 0; i <objects.length; i++)
+        {
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+        fill("#b71c1c");
+        percent = floor(objects[i].confidence*100);
+        text(objects[i].label + " " + percent + "% " ,  objects[i].x + 15 , objects[i].y + 15);
+        noFill();
+        stroke("#b71c1c");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function gotResult(error, results)
+{
+   if(error)
+   {
+    console.log(error);
+   }
+   else
+   {
+    console.log(results);
+    objects = results;
+   }
+}
+
+function back()
+{
+
+}
